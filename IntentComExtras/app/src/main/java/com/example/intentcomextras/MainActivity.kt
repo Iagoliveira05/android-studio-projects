92cfdefb16d3a929d1628eb4b5c27a336f1482e8
Iago de Oliveira Andrade <iago2011.andrade@hotmail.com>
2023-12-30 17:51:45 -0300

    Finalização IntentComExtras

--- IntentComExtras/app/src/main/java/com/example/intentcomextras/MainActivity.kt
+++ IntentComExtras/app/src/main/java/com/example/intentcomextras/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.intentcomextras
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.intentcomextras.databinding.ActivityMainBinding
@@ -11,7 +12,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.buttonOk.setOnClickListener {
+            val nome = binding.editNome.text.toString()
 
+            val i = Intent(this, MainActivity2::class.java)
+            i.putExtra("nome", nome)
+            startActivity(i)
+        }
 
     }
 }
