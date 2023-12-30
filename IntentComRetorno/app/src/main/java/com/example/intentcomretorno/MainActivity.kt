6ac7227cd293099926d71a0ada2aa59c28f41c7d
Iago de Oliveira Andrade <iago2011.andrade@hotmail.com>
2023-12-30 18:30:42 -0300

    Finalizado IntentComRetorno (Enviando e coletando informações entre Activity)

---
+++ IntentComRetorno/app/src/main/java/com/example/intentcomretorno/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.intentcomretorno
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.intentcomretorno.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var result: ActivityResultLauncher<Intent>
+    private var nome = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonMudarNome.setOnClickListener {
+            val i = Intent(this, MainActivity2::class.java)
+            i.putExtra("nome", nome)
+            //startActivity(i)
+            result.launch(i)
+        }
+
+        result = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.data != null && it.resultCode == 1) {
+                nome = it.data?.getStringExtra("nome").toString()
+                binding.textNome.setText("Olá ${nome}")
+            } else if (it.data != null && it.resultCode == 2){
+                Toast.makeText(applicationContext, "Operação cancelada!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Erro ao atualizar o nome!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ IntentComRetorno/app/src/main/java/com/example/intentcomretorno/MainActivity2.kt
@@ -0,0 +1,31 @@
+package com.example.intentcomretorno
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.intentcomretorno.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val i = intent
+        val nome = i.extras?.getString("nome")
+
+        binding.editNome.setText(nome)
+
+        binding.buttonOk.setOnClickListener {
+            i.putExtra("nome", binding.editNome.text.toString())
+            setResult(1, i)
+            finish()
+        }
+
+        binding.buttonCancelar.setOnClickListener {
+            i.putExtra("nome", binding.editNome.text.toString())
+            setResult(2, i)
+            finish()
+        }
+    }
+}
