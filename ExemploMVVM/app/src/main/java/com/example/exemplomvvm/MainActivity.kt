2cad64dfa4d1f6d3a6d26272414152c6c41cdbf0
Iago de Oliveira Andrade <iago2011.andrade@hotmail.com>
2024-03-05 16:46:59 -0300

    Exemplo MVVM

---
+++ ExemploMVVM/app/src/main/java/com/example/exemplomvvm/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.exemplomvvm
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.exemplomvvm.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        viewModel.welcome().observe(this, Observer {
+            binding.textWelcome.text = it
+        })
+    }
+}
